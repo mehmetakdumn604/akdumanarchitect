0fd40d2fc80dff3c05ebd183e6662e49734cd014
mehmetakdumn604 <makdumn604@gmail.com>
2025-02-23 15:05:34 +0300

    added purchase package

--- lib/src/core/constants/app/app_constants.dart
+++ lib/src/core/constants/app/app_constants.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../../common/viewModels/language_view_model.dart';
-import '../../../common/viewModels/theme_view_model.dart';
 
 // Uygulamayla ilgili temel veriler
 class AppConstants {
@@ -10,9 +9,7 @@ class AppConstants {
   static const fontFamily = 'fontFamily';
 
   static final defaultProviders = [
-    ChangeNotifierProvider<ThemeViewModel>(
-      create: (context) => ThemeViewModel(),
-    ),
+ 
     ChangeNotifierProvider<LanguageViewModel>(
       create: (context) => LanguageViewModel(),
     ),
