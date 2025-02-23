6ba4f5747ecd622349ee2b086fd6866d76fb4f3e
mehmetakdumn604 <makdumn604@gmail.com>
2025-02-23 14:21:32 +0300

    removed unused import and files

--- lib/arch/main.dart
+++ lib/arch/main.dart
@@ -2,18 +2,14 @@ const mainPage = """
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:../src/core/services/local/local_service.dart';
-import 'package:../src/core/services/purchase/purchase_manager.dart';
 import 'package:provider/provider.dart';
 
-import 'src/common/viewModels/language_view_model.dart';
-import 'src/common/viewModels/theme_view_model.dart';
 import 'src/core/exports/constants_exports.dart';
-import 'src/core/services/language/languages/l10n.dart';
+import 'src/core/services/local/local_service.dart';
 import 'src/core/services/navigation/navigation_route.dart';
 import 'src/core/services/navigation/navigation_service.dart';
+import 'src/core/services/purchase/purchase_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -51,7 +47,6 @@ class MyApp extends StatelessWidget {
           theme: ThemeConstants.lightTheme,
           darkTheme: ThemeConstants.darkTheme,
           debugShowCheckedModeBanner: false,
-          themeMode: context.watch<ThemeViewModel>().themeMode,
           initialRoute: NavigationConstants.home,
           onGenerateRoute: NavigationRoute.instance.generateRoute,
           navigatorKey: NavigationService.instance.navigatorKey,
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,10 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:makdumanarchitect/src/core/services/local/local_service.dart';
-import 'package:makdumanarchitect/src/core/services/purchase/purchase_manager.dart';
+import 'src/core/services/local/local_service.dart';
+import 'src/core/services/purchase/purchase_manager.dart';
 import 'package:provider/provider.dart';
 
-import 'src/common/viewModels/theme_view_model.dart';
 import 'src/core/exports/constants_exports.dart';
 import 'src/core/services/navigation/navigation_route.dart';
 import 'src/core/services/navigation/navigation_service.dart';
@@ -47,7 +46,6 @@ class MyApp extends StatelessWidget {
           theme: ThemeConstants.lightTheme,
           darkTheme: ThemeConstants.darkTheme,
           debugShowCheckedModeBanner: false,
-          themeMode: context.watch<ThemeViewModel>().themeMode,
           initialRoute: NavigationConstants.home,
           onGenerateRoute: NavigationRoute.instance.generateRoute,
           navigatorKey: NavigationService.instance.navigatorKey,
